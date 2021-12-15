2898ed64f7c5d3c483665b6f847a752a0495cd92
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-12-15 14:17:28 -0300

    Added cart button

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,9 +1,21 @@
 class CartsController < ApplicationController
 
+  def show
+    @user = current_user
+  end
+
   def create
+    @cart = Cart.new(cart_params)
+    redirect_to cart_path(current_user)
   end
 
   def update
+  end
+
+  private
+
+  def cart_params
+    params.require(:cart).permit(:user)
   end
 
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+    @cart = Cart.new
   end
 
   def speakers
