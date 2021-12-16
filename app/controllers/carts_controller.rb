f7312eb4620f80f8b791d2083ae33acae1ca575b
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-12-16 10:33:06 -0300

    added conditional for visitor in navbar and footer

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,10 +3,9 @@ class CartsController < ApplicationController
 
   def show
     @user = current_user
-    @cart = @user.carts.first
-    @carpros = Carpro.where(cart: @cart)
-
-    @sum = carpros_sum(@carpros)
+      @cart = @user.carts.first
+      @carpros = Carpro.where(cart: @cart)
+      @sum = carpros_sum(@carpros)
   end
 
   def create
