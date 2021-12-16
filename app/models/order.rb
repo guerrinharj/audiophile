93ea1b2f245d7c03974f01a5c0eb8b1dff7ea148
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-12-15 22:49:54 -0300

    added order validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,13 @@
 class Order < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :email, presence: true
+  validates :phone_number, presence: true
+  validates :zipcode, presence: true
+  validates :city, presence: true
+  validates :country, presence: true
+  validates :emoney_number, presence: true
+  validates :emoney_pin, presence: true
 end
