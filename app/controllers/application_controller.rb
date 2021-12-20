1af299a46a1a05ab2ba555ad84b70c4dad7a0d7c
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-12-20 14:51:08 -0300

    fixed navbar conditional and added style to devise pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-  @categories = Category.all
+  before_action :authenticate_user!, :nav_categories
+
+
+  def nav_categories
+    @categories = Category.all
+  end
 end
