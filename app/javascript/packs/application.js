2199bcefc73708deb67745aedbe51e366f801b63
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-12-21 12:20:40 -0300

    added function parenthesis in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,5 +31,5 @@ import { mobileMenu } from './mobile-menu';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  mobileMenu
+  mobileMenu();
 });
